ee47afcf375e1e69fb9396af054b9c18a2267b7a
Michael Borromeo <mbborromeo@gmail.com>
2019-08-10 22:25:08 -0700

    delete unused files

--- src/app/_mock-busOrganisations.ts
+++
@@ -1,5 +0,0 @@
-/* File not used now that we are using in-memory-data.service.ts */
-import { BusOrganisation } from './busOrganisation';
-import { data } from '../assets/bus-services-data.json';
-
-export const BUSORGANISATIONS: BusOrganisation[] = data;
